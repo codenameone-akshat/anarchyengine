d41f8236cb3278e10ffa64f78508de9a32fbd84f
Akshat Thukral <code.akshat.thukral@gmail.com>
2020-07-09 06:19:30 +0530

    [Framework] Adding class helper marcos. Used to auto declare the default members and auto declare the getter and setter for the property.

---
+++ Anarchy/Source/Framework/Includes/ClassHelpers.h
@@ -0,0 +1,37 @@
+#ifndef _CLASS_HELPERS_H
+
+namespace anarchy::framework
+{
+
+#define DECLARE_DEFAULT_CLASSMEMBERS(className)\
+public:\
+className() = default;\
+~className() = default;\
+className(const className&) = default;\
+className(className&&) = default;\
+className& operator=(const className&) = default;
+
+
+#define DECLARE_DELETED_CLASSMEMBERS(className)\
+public:\
+className() = delete;\
+~className() = delete;\
+className(const className&) = delete;\
+className(className&&) = delete;\
+className& operator=(const className&) = delete;
+
+#define DECLARE_GETTERSETTER(type, varName, propertyName) \
+inline type Get##propertyName() { return m_##varName; } \
+inline void Set##propertyName(type& arg) { m_##varName = arg; } 
+
+#define DECLARE_PROPERTY(type, varName, propertyName, defaultVal) \
+public: \
+DECLARE_GETTERSETTER(type, varName, propertyName) \
+private: \
+type m_##varName = defaultVal; 
+
+#define DECLARE_DEFAULT_PROPERTY(type, varName, propertyName) \
+DECLARE_PROPERTY(type, varName, propertyName, {}) 
+
+}
+#endif // !_CLASS_HELPERS_H
