9e769da944c67d8bbd428f7445d3914cbf726531
codenameone-akshat <code.akshat.thukral@gmail.com>
2021-01-02 13:55:34 +0530

    Adding Sandbox interface in its own namespace

--- Anarchy/Source/Game/SandboxInterface.cpp
+++ Anarchy/Source/Game/SandboxInterface.cpp
@@ -5,36 +5,39 @@ namespace anarchy
 {
 	// Instance Creation
 	GAME_SPECIFIC_INTERFACE_CREATEINSTANCE_IMPL_BEGIN
-		GameSpecificInterface::SetInstance(std::make_shared<SandboxInterface>());
+		GameSpecificInterface::SetInstance(std::make_shared<sandbox::SandboxInterface>());
 	GAME_SPECIFIC_INTERFACE_CREATEINSTANCE_IMPL_END
+	
+	namespace sandbox
+	{
+		void SandboxInterface::Initialize()
+		{
+			SUPER_CALL(super::Initialize();)
+		}
 
-	void SandboxInterface::Initialize()
-	{
-		SUPER_CALL(super::Initialize();)
-	}
-	
-	void SandboxInterface::Update()
-	{
-		SUPER_CALL(super::Update();)
-	}
-	
-	void SandboxInterface::PreRender()
-	{
-		SUPER_CALL(super::PreRender();)
-	}
-	
-	void SandboxInterface::Render()
-	{
-		SUPER_CALL(super::Render();)
-	}
-	
-	void SandboxInterface::PostRender()
-	{
-		SUPER_CALL(super::PostRender();)
-	}
-	
-	void SandboxInterface::Shutdown()
-	{
-		SUPER_CALL(super::Shutdown();)
+		void SandboxInterface::Update()
+		{
+			SUPER_CALL(super::Update();)
+		}
+
+		void SandboxInterface::PreRender()
+		{
+			SUPER_CALL(super::PreRender();)
+		}
+
+		void SandboxInterface::Render()
+		{
+			SUPER_CALL(super::Render();)
+		}
+
+		void SandboxInterface::PostRender()
+		{
+			SUPER_CALL(super::PostRender();)
+		}
+
+		void SandboxInterface::Shutdown()
+		{
+			SUPER_CALL(super::Shutdown();)
+		}
 	}
 }
--- Anarchy/Source/Game/SandboxInterface.h
+++ Anarchy/Source/Game/SandboxInterface.h
@@ -15,23 +15,26 @@ namespace anarchy
 {
 	GAME_SPECIFIC_INTERFACE_CREATEINSTANCE_IMPL_DECLARE;
 
-	class SandboxInterface : public GameSpecificInterface
+	namespace sandbox
 	{
-		DECLARE_SUPERCLASS(GameSpecificInterface);
-		VIRTUAL_DTOR_GUARD(GameSpecificInterface);
-	public:
-		virtual void Initialize() override;
+		class SandboxInterface : public GameSpecificInterface
+		{
+			DECLARE_SUPERCLASS(GameSpecificInterface);
+			VIRTUAL_DTOR_GUARD(GameSpecificInterface);
+		public:
+			virtual void Initialize() override;
 
-		virtual void Update() override;
+			virtual void Update() override;
 
-		virtual void PreRender() override;
+			virtual void PreRender() override;
 
-		virtual void Render() override;
+			virtual void Render() override;
 
-		virtual void PostRender() override;
+			virtual void PostRender() override;
 
-		virtual void Shutdown() override;
-	};
+			virtual void Shutdown() override;
+		};
+	}
 }
 
 #endif // !_SANDBOX_INTERFACE_H_
