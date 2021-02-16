d16ea5c17cadf930d539c3ec056fbfdefb26f431
codenameone-akshat <code.akshat.thukral@gmail.com>
2021-02-17 02:28:05 +0530

    [Utils] Fixing bug for 0 milliseconds in scoped timer.

--- Anarchy/Source/Utils/Time/ScopedTimer.cpp
+++ Anarchy/Source/Utils/Time/ScopedTimer.cpp
@@ -16,15 +16,15 @@ namespace anarchy
 		m_end = std::chrono::high_resolution_clock::now();
 		int64 time_duration = std::chrono::duration_cast<std::chrono::milliseconds>(m_end - m_start).count();
         
-        int64 milliseconds = 0;
+        int64 milliseconds = time_duration;
         int64 seconds = 0;
         int64 minutes = 0;
 		string message = m_tag + fmt::format(" Operation took {} miliseconds.", milliseconds);
 
 		if (time_duration > 1000)
 		{
-			milliseconds = time_duration % 1000;
-			seconds = time_duration / 1000;
+			seconds = milliseconds / 1000;
+			milliseconds = milliseconds % 1000;
 			message = m_tag + fmt::format(" Operation took {} seconds {} miliseconds.", seconds, milliseconds);
 		}
 		if (seconds > 60)
