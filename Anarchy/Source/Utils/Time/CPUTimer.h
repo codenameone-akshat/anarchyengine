fc49b49766e70f49c6eb2db7409c4dea29904cd4
codenameone-akshat <code.akshat.thukral@gmail.com>
2021-03-26 03:16:23 +0530

    [Utils] Adding reset tag to CPUTimer.

--- Anarchy/Source/Utils/Time/CPUTimer.h
+++ Anarchy/Source/Utils/Time/CPUTimer.h
@@ -17,11 +17,13 @@ namespace anarchy
 
         void Start();
         void Stop();
+        
+        inline void ResetTag(string tag) { m_tag = tag; };
 
     private:
         std::chrono::high_resolution_clock::time_point m_start;
         std::chrono::high_resolution_clock::time_point m_end;
-        string m_tag = "UnTagged";
+        string m_tag;
     };
 }
 
